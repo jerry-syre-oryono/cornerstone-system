a8bb82f8e8d2dff039973e03c44b816cb81a9bf0
jerry syre <jeremyoryono@gmail.com>
2026-03-31 18:44:23 +0300

    feat: initialize Django project settings with environment-based configuration and Redis caching

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,7 +143,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Add to bottom of file
-import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
